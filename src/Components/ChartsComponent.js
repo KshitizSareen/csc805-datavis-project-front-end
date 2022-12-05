80451c5eb6249d65389eda15b1de45bf9f99ac60
KshitizSareen <kshitizsareen709@gmail.com>
2022-12-05 03:14:01 -0800

    Add tooltips to points in line chart

--- src/Components/ChartsComponent.js
+++ src/Components/ChartsComponent.js
@@ -5,9 +5,9 @@ import { Chart } from "react-google-charts";
 
 export default function ChartsComponent({chartsByListing,chartsByLocation,chartsByCategory}){
     
-    var margin = {top: 20, right: 75, bottom: 20, left: 75};
+    var margin = {top: 50, right: 100, bottom: 50, left: 100};
     const width = window.parent.outerWidth/2.5;
-    const height = window.parent.outerHeight/2.5;
+    const height = window.parent.outerHeight/3;
     const svgWidth = width + margin.left + margin.right;
     const svgHeight =  height + margin.top + margin.bottom;
 
@@ -51,10 +51,29 @@ export default function ChartsComponent({chartsByListing,chartsByLocation,charts
         horizontalBarChartElem.selectAll('*').remove();
         var horizontalBarChartSvg = horizontalBarChartElem.append("g").attr("transform",`translate(${margin.left},${margin.top})`);
         horizontalBarChartSvg.append("g").attr("class","xAxis")
-        .attr("transform","translate(0,"+height+")");
+        .attr("transform","translate(0,"+height+")")
+        
+        horizontalBarChartSvg.append("text")
+        .attr("y", height+40)
+        .attr("x", width/2)
+        .attr("text-anchor", "middle")
+        .text("Location");
+
+        horizontalBarChartSvg.append("text")
+        .attr("transform","rotate(-90)")
+        .attr("x", -(height/2))
+        .attr("y", -40)
+        .attr("text-anchor", "middle")
+        .text("Price");
     
         horizontalBarChartSvg.append("g")
         .attr("class","yAxis")
+
+        horizontalBarChartElem.append("text")
+   .attr("x", width/2)
+   .attr("y", 35)
+   .attr("font-size", "12px")
+   .text("Location VS Price")
 
     }
 
@@ -68,6 +87,25 @@ export default function ChartsComponent({chartsByListing,chartsByLocation,charts
     
         verticalBarChartSvg.append("g")
         .attr("class","yAxis")
+
+        verticalBarChartElem.append("text")
+        .attr("x", width/2)
+        .attr("y", 35)
+        .attr("font-size", "12px")
+        .text("Price VS Category")
+
+        verticalBarChartSvg.append("text")
+        .attr("y", height+40)
+        .attr("x", width/2)
+        .attr("text-anchor", "middle")
+        .text("Price");
+
+        verticalBarChartSvg.append("text")
+        .attr("transform","rotate(-90)")
+        .attr("x", -(height/2))
+        .attr("y", -80)
+        .attr("text-anchor", "middle")
+        .text("Category");
     }
 
     function initializeLineChart()
@@ -79,6 +117,25 @@ export default function ChartsComponent({chartsByListing,chartsByLocation,charts
         lineSvg.append("g").attr("class","yAxis");
     
         lineSvg.append('g').attr('class','lines');
+
+        lineChartElem.append("text")
+        .attr("x", width/2)
+        .attr("y", 35)
+        .attr("font-size", "12px")
+        .text("Listing Data")
+
+        lineSvg.append("text")
+        .attr("y", height+40)
+        .attr("x", width/2)
+        .attr("text-anchor", "middle")
+        .text("Listing ID");
+
+        lineSvg.append("text")
+        .attr("transform","rotate(-90)")
+        .attr("x", -(height/2))
+        .attr("y", -40)
+        .attr("text-anchor", "middle")
+        .text("Value");
     }
 
     useEffect(()=>{
@@ -306,6 +363,7 @@ else{
             })
         }
         data=[priceData,valueData]
+        const circleData = priceData.concat(valueData)
         console.log(data);
         lineXScale.domain([minX, maxX]);
         lineYScale.domain([minY,maxY]);
@@ -336,6 +394,19 @@ else{
             return lineYScale(d['value']);
           })(d)
         })
+
+        var circles=lineSvg.selectAll(".circle").data(circleData)
+
+        circles.enter().append("circle")
+        .attr("class","circle")
+        .merge(circles)
+        .transition(t)
+        .attr("fill","red")
+        .attr("cx", d => lineXScale(d.index))
+        .attr("cy", d => lineYScale(d.value))
+        .attr("r", circleRadius)
+      
+        circles.exit().remove();
 
         lines.exit().remove();
     }
@@ -385,13 +456,17 @@ else{
             overflow: 'hidden'
         }}>
             <div style={{
-                display: 'flex'
+                display: 'flex',
+                width: window.parent.innerWidth,
+                justifyContent: 'space-between'
             }}>
             <svg ref={horizontalBarChartRef} width={svgWidth} height={svgHeight}/>
             <svg ref={verticalBarChartRef} width={svgWidth} height={svgHeight}/>
             </div>
             <div style={{
-                display: 'flex'
+                display: 'flex',
+                width: window.parent.innerWidth,
+                justifyContent: 'space-between'
             }}>
             <svg ref={lineChartRef} width={svgWidth} height={svgHeight}/>
             <Chart
